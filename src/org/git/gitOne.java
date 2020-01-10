c41724b5f302f3318a85ef62c6715f110f18f23c
mathan55 <mathankumarmk025@gmail.com>
2020-01-10 11:52:48 +0530

    new code added for login

---
+++ src/org/git/gitOne.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class gitOne {
+	
+	public static void main(String[] args) {
+		String s ="Java Technology";
+		//To find the length of string
+		int a=s.length();
+		System.out.println(a);
+		//To pickup the particular value
+		char b=s.charAt(13);
+		System.out.println(b);
+
+}
+}
